f4251e32f8121fffe0ba003c8d8b738f54ea79d2
Igor <igara8880@gmail.com>
2023-04-06 18:20:16 +0300

    complete task6

--- StudentCppRevisionTask06Project/task06.cpp
+++ StudentCppRevisionTask06Project/task06.cpp
@@ -23,5 +23,11 @@
 */
 
 bool task06(int x1, int y1, int x2, int y2) {
+	if (x1 < 1 || x1 > 8 || x2 < 1 || x2 > 8 || y1 < 1 || y1 > 8 || y2 < 1 || y2 > 8) {
+		return false;
+	}
+	if ((x1 == x2 + 2 && y1 == y2 + 1 || y1 == y2 - 1) || (x1 == x2 - 2 && y1 == y2 + 1 || y1 == y2 - 1) || (y1 == y2 + 2 && x1 == x2 + 1 || x1 == x2 - 1) || (y1 == y2 - 2 && x1 == x2 + 1 || x1 == x2 - 1)) {
+		return true;
+	}
 	return false;
 }
