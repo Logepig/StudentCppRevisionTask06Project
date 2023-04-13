533e6dbb72fa8d60d314ad8440d94d9b5ba38e14
Lenovo <ilia_vinok@mail.ru>
2023-04-13 15:30:17 +0300

    TaskX

--- StudentCppRevisionTask06Project/task05.cpp
+++ StudentCppRevisionTask06Project/task05.cpp
@@ -25,7 +25,7 @@ bool task05(int x1, int y1, int x2, int y2) {
 	if (x1 < 1 || x1 > 8 || x2 < 1 || x2 > 8 || y1 < 1 || y1 > 8 || y2 < 1 || y2 > 8) {
 		return false;
 	}
-	if ((y1 - x1 == y2 - x2) || (x1 + y1 == x2 + y2) {
+	if (((y1 - x1) == (y2 - x2)) || ((x1 + y1) == (x2 + y2))) {
 		return true;
 	}
 	return false;
--- StudentCppRevisionTask06Project/taskX.cpp
+++ StudentCppRevisionTask06Project/taskX.cpp
@@ -22,5 +22,11 @@
 */
 
 bool taskX(int x1, int y1, int x2, int y2) {
-	return true;
+	if (x1 < 1 || x1 > 8 || x2 < 1 || x2 > 8 || y1 < 1 || y1 > 8 || y2 < 1 || y2 > 8) {
+		return false;
+	}
+	if ((y1 - x1 == y2 - x2) || (x1 + y1 == x2 + y2) || (x1 == x2 && y1 != y2) || (x1 != x2 && y1 == y2)) {
+		return true;
+	}
+	return false;
 }
