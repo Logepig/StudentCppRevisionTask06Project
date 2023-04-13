322ceca235f69357561c146ef6e87cbfdd0580a9
Lenovo <ilia_vinok@mail.ru>
2023-04-13 15:23:58 +0300

    Task5

--- StudentCppRevisionTask06Project/task05.cpp
+++ StudentCppRevisionTask06Project/task05.cpp
@@ -25,5 +25,8 @@ bool task05(int x1, int y1, int x2, int y2) {
 	if (x1 < 1 || x1 > 8 || x2 < 1 || x2 > 8 || y1 < 1 || y1 > 8 || y2 < 1 || y2 > 8) {
 		return false;
 	}
-	return true;
+	if ((y1 - x1 == y2 - x2) || (x1 + y1 == x2 + y2) {
+		return true;
+	}
+	return false;
 }
